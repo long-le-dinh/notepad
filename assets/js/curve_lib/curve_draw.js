b42a1b80dbdccb15afc5f081dd77cad9ae1e6416
LONG <ledinhlong24102000@gmail.com>
2024-07-06 00:08:18 +0700

    lan 1

---
+++ assets/js/curve_lib/curve_draw.js
@@ -0,0 +1,133 @@
+/*!	Curve function for canvas 3.0.1
+ *	(c) Epistemex 2013-2018, 2024
+ *	License: MIT
+ */
+
+'use strict';
+
+/**
+ * Draws a cardinal spline through given point array. Points must be arranged
+ * as: [x1, y1, x2, y2, ..., xn, yn]. It adds the points to the current path.
+ *
+ * There must be a minimum of two points in the input array but the function
+ * is only useful where there are three points or more.
+ *
+ * The method continues previous path of the context. If you don't want that
+ * then you need to use moveTo() with the first point from the input array.
+ *
+ * The points for the cardinal spline are returned as a new array.
+ *
+ * @param {CanvasRenderingContext2D} ctx - context to use
+ * @param {Array} points - point array
+ * @param {Number} [tension=0.5] - tension. Typically between [0.0, 1.0] but can be exceeded
+ * @param {Number} [numOfSeg=25] - number of segments between two points (line resolution)
+ * @param {Boolean} [close=false] - Close the ends making the line continuous
+ * @returns {Float32Array} New array with the calculated points that was added to the path
+ */
+function curve(ctx, points, tension, numOfSeg, close) {
+
+	if (typeof points === "undefined" || points.length < 2) {
+		return new Float32Array(0);
+	}
+
+	// options or defaults
+	tension = typeof tension === "number" ? tension : 0.5;
+	numOfSeg = (typeof numOfSeg === "number" ? numOfSeg : 25) | 0;
+
+	if (numOfSeg < 1) {
+		throw new Error('Number of segments cannot be less than one.');
+	}
+
+	let ptsClone;                                                       // for cloning point array
+	let i = 1;
+	let l = points.length;
+	let resPos = 0;
+	let cachePtr = 4;
+
+	const resLength = (l - 2) * numOfSeg + 2 + (closed ? 2 * numOfSeg : 0);
+	const result = new Float32Array(resLength);
+	const cache = new Float32Array((numOfSeg + 2) << 2);
+
+	ptsClone = points.slice(0);
+
+	if (closed) {
+		ptsClone.unshift(points[l - 1]);                                // insert end point as first point
+		ptsClone.unshift(points[l - 2]);
+		ptsClone.push(points[0], points[1]);                            // first point as last point
+	}
+	else {
+		ptsClone.unshift(points[1]);                                    // copy 1. point and insert at beginning
+		ptsClone.unshift(points[0]);
+		ptsClone.push(points[l - 2], points[l - 1]);                    // duplicate end-points
+	}
+
+	// cache inner-loop calculations as they are based on t alone
+	cache[0] = 1;                                                       // 1,0,0,0
+
+	for(; i < numOfSeg; i++) {
+		const st = i / numOfSeg;
+		const st2 = st * st;
+		const st3 = st2 * st;
+		const st23 = st3 * 2;
+		const st32 = st2 * 3;
+
+		cache[cachePtr++] = st23 - st32 + 1;                            // c1
+		cache[cachePtr++] = st32 - st23;                                // c2
+		cache[cachePtr++] = st3 - 2 * st2 + st;                         // c3
+		cache[cachePtr++] = st3 - st2;                                  // c4
+	}
+
+	cache[++cachePtr] = 1;                                              // 0,1,0,0
+
+	// calc. points
+
+	fill(ptsClone, cache, l, tension);
+
+	if (closed) {
+		//l = points.length;
+		ptsClone = [];
+		ptsClone.push(points[l - 4], points[l - 3],
+			points[l - 2], points[l - 1],                               // second last and last
+			points[0], points[1],
+			points[2], points[3]);                                      // first and second
+		fill(ptsClone, cache, 4, tension);
+	}
+
+	function fill(pts, cache, l, tension) {
+		for(let i = 2; i < l; i += 2) {
+			const pt1 = pts[i];                                         // x1
+			const pt2 = pts[i + 1];                                     // y1
+			const pt3 = pts[i + 2];                                     // x2
+			const pt4 = pts[i + 3];                                     // y2
+
+			const t1x = (pt3 - pts[i - 2]) * tension;                   // x2-x0
+			const t1y = (pt4 - pts[i - 1]) * tension;                   // y2-y0
+			const t2x = (pts[i + 4] - pt1) * tension;                   // x3-x1
+			const t2y = (pts[i + 5] - pt2) * tension;                   // y3-y1
+			let c = 0, c1, c2, c3, c4;
+
+			for(let t = 0; t < numOfSeg; t++) {
+				c1 = cache[c++];
+				c2 = cache[c++];
+				c3 = cache[c++];
+				c4 = cache[c++];
+
+				result[resPos++] = c1 * pt1 + c2 * pt3 + c3 * t1x + c4 * t2x;
+				result[resPos++] = c1 * pt2 + c2 * pt4 + c3 * t1y + c4 * t2y;
+			}
+		}
+	}
+
+	// append last point
+	l = closed ? 0 : points.length - 2;
+	result[resPos++] = points[l++];
+	result[resPos] = points[l];
+
+	// add lines to path
+	for(i = 0, l = result.length; i < l; i += 2)
+		ctx.lineTo(result[i], result[i+1]);
+
+	return result
+}
+
+if (typeof exports !== "undefined") exports.curve = curve;
